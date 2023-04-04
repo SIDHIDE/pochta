3ac3264e2e74d80cb0a2f0ed440f538519894f50
SIDHIDE <vlad-danilin.123@yandex.ru>
2023-04-04 22:20:19 +0300

    Update pochta.php

--- pochta.php
+++ pochta.php
@@ -55,10 +55,68 @@ function postrussia_shipping_method()
                         'type'    => 'checkbox',
                         'label'   => __('Включить доставку Почтой России', 'woocommerce'),
                         'default' => 'yes'
+                    ),
+                    'pochta_delivery_type' => array(
+                    'title'       => __( 'Тип доставки', 'woocommerce' ),
+                    'type'        => 'select',
+                    'description' => __( 'Выберите тип доставки', 'woocommerce' ),
+                    'default'     => 'courier',
+                    'options'     => array(
+                    'courier' => __( 'Курьером', 'woocommerce' ),
+                    'pvz'     => __( 'До ПВЗ', 'woocommerce' ),
                     )
                 );
             }
+        // Existing code for the class
+        /**
+         * Calculate shipping cost to PVZ
+         *
+         * @param array $package Package information
+         * @return void
+         */
 
+                private function calculate_shipping_to_pvz( $package ) {
+                    // Определяем параметры доставки
+                    $weight = $this->get_package_weight( $package );
+                    $volume = $this->get_package_volume( $package );
+
+                    // Формируем тело запроса к API Почты России
+                    $request = array(
+                        'weight' => $weight,
+                        'length' => $volume['length'],
+                        'width'  => $volume['width'],
+                        'height' => $volume['height'],
+                        'pvz'    => true,
+                    );
+
+                    // Отправляем запрос к API Почты России
+                    $response = wp_remote_post( 'https://otpravka-api.pochta.ru/1.0/tariff', array(
+                        'headers' => array(
+                            'Content-Type' => 'application/json',
+                            'Authorization' => 'AccessToken ' . $this->settings['pochta_token'],
+                        ),
+                        'body' => json_encode( $request ),
+                    ) );
+
+                    // Обрабатываем ответ от API Почты России
+                    if ( is_wp_error( $response ) ) {
+                        return false;
+                    } else {
+                        $data = json_decode( wp_remote_retrieve_body( $response ), true );
+                        if ( isset( $data['total-rate'] ) ) {
+                            $rate = $data['total-rate'];
+                            $label = 'Доставка до ПВЗ Почты России (' . $data['delivery-time'] . ' дней)';
+                            $this->add_rate( array(
+                                'id'    => $this->id . '_pvz',
+                                'label' => $label,
+                                'cost'  => $rate,
+                            ) );
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+                    
             /**
              * Calculate shipping cost
              *
@@ -66,59 +124,55 @@ function postrussia_shipping_method()
              * @param mixed $package
              * @return void
              */
+                    
             public function calculate_shipping( $package = array() ) {
+            // Определение параметров заказа
+            $weight = $this->get_cart_weight();
+            $volume = $this->get_cart_volume();
+            $length = $this->get_cart_length();
+            $width = $this->get_cart_width();
+            $height = $this->get_cart_height();
+            $destination = $package['destination'];
 
-    // Определение параметров заказа
-    $weight = $this->get_cart_weight();
-    $volume = $this->get_cart_volume();
-    $length = $this->get_cart_length();
-    $width = $this->get_cart_width();
-    $height = $this->get_cart_height();
-    $destination = $package['destination'];
-    
-    // Получение ключа доступа к API Почты России
-    $access_token = 'YOUR_ACCESS_TOKEN';
-    
-    // Создание экземпляра клиента Guzzle
-    $client = new \GuzzleHttp\Client();
-    
-    // Отправка запроса к API Почты России
-    $response = $client->request('POST', 'https://otpravka-api.pochta.ru/1.0/tariff', [
-        'headers' => [
-            'Authorization' => 'AccessToken ' . $access_token,
-            'Content-Type' => 'application/json;charset=UTF-8',
-        ],
-        'json' => [
-            'object' => [
-                'weight' => $weight,
-                'volume' => $volume,
-                'length' => $length,
-                'width' => $width,
-                'height' => $height,
-            ],
-            'destination' => $destination,
-        ],
-    ]);
+            // Получение ключа доступа к API Почты России
+            $access_token = 'YOUR_ACCESS_TOKEN';
 
-    // Обработка ответа от API Почты России
-    $data = json_decode( $response->getBody()->getContents(), true );
-    $price = $data['total-rate'];
-    
-    // Установка стоимости доставки
-    $rate = array(
-        'id' => $this->id,
-        'label' => $this->title,
-        'cost' => $price,
-        'calc_tax' => 'per_order'
-    );
-    
-    $this->add_rate( $rate );
-}
+            // Создание экземпляра клиента Guzzle
+            $client = new \GuzzleHttp\Client();
+
+            // Отправка запроса к API Почты России
+            $response = $client->request('POST', 'https://otpravka-api.pochta.ru/1.0/tariff', [
+                'headers' => [
+                    'Authorization' => 'AccessToken ' . $access_token,
+                    'Content-Type' => 'application/json;charset=UTF-8',
+                ],
+                'json' => [
+                    'object' => [
+                        'weight' => $weight,
+                        'volume' => $volume,
+                        'length' => $length,
+                        'width' => $width,
+                        'height' => $height,
+                    ],
+                    'destination' => $destination,
+                ],
+            ]);
+
+            // Обработка ответа от API Почты России
+            $data = json_decode( $response->getBody()->getContents(), true );
+            $price = $data['total-rate'];
+
+            // Установка стоимости доставки
+            $rate = array(
+                'id' => $this->id,
+                'label' => $this->title,
+                'cost' => $price,
+                'calc_tax' => 'per_order'
+            );
+
+            $this->add_rate( $rate );
+        }
     /**/
-            public function calculate_shipping($package = array())
-            {
-                // Not implemented in this example
-            }
         }
     }
     
